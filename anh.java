9e2629462620a8cc8bd355eb80708a7416ecb467
Tuan Anh <letuananh27102006@gmail.com>
2025-02-06 21:08:12 +0700

    upload source code

---
+++ anh.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class anh {
+    public static void main(String[] args) {
+        
+    
+
+    Scanner myObj = new Scanner(System.in);
+    //Để nhận vào
+    System.out.println("Nhập vào đây nào bé");
+    String userName = myObj.nextLine();
+    System.out.println("tên của bạn là "+userName);
+    
+    
+}}
